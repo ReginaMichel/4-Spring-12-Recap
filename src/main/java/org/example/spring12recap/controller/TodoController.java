15ff3902d3816ad514c5590af144e83e31ad64c1
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-07-22 18:39:29 +0200

    Added getAll method to Controller and Service.

--- src/main/java/org/example/spring12recap/controller/TodoController.java
+++ src/main/java/org/example/spring12recap/controller/TodoController.java
@@ -1,9 +1,25 @@
 package org.example.spring12recap.controller;
 
+import org.example.spring12recap.model.Todo;
+import org.example.spring12recap.service.TodoService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todo")
 public class TodoController {
+
+    private final TodoService service;
+
+    public TodoController(TodoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Todo> getAll() {
+        return service.getAll();
+    }
 }
--- src/main/java/org/example/spring12recap/service/TodoService.java
+++ src/main/java/org/example/spring12recap/service/TodoService.java
@@ -1,12 +1,19 @@
 package org.example.spring12recap.service;
 
+import org.example.spring12recap.model.Todo;
 import org.example.spring12recap.repository.TodoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
     private final TodoRepository repo;
     public TodoService(TodoRepository repo) {
         this.repo = repo;
+    }
+
+    public List<Todo> getAll() {
+        return repo.findAll();
     }
 }
