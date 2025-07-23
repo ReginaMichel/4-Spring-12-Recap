bc1fa8b5195448280a1853886d3352ccc5dbdfc3
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-07-23 14:08:11 +0200

    Added monitoring and added History.

--- src/main/java/org/example/spring12recap/service/TodoService.java
+++ src/main/java/org/example/spring12recap/service/TodoService.java
@@ -1,19 +1,29 @@
 package org.example.spring12recap.service;
 
+import lombok.Getter;
+import org.example.spring12recap.model.Change;
+import org.example.spring12recap.model.ChangeType;
 import org.example.spring12recap.model.Todo;
 import org.example.spring12recap.model.TodoDTO;
 import org.example.spring12recap.repository.TodoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @Service
 public class TodoService {
     private final TodoRepository repo;
     private final IdService idService;
+    @Getter
+    private final LinkedHashMap<Integer, Change> history;
+    private int counter;
+
     public TodoService(TodoRepository repo, IdService idService) {
         this.repo = repo;
         this.idService = idService;
+        history = new LinkedHashMap<>();
+        counter = 0;
     }
 
     public List<Todo> getAll() {
@@ -25,6 +35,7 @@ public class TodoService {
         repo.save(new Todo(newID,
                 todoDTO.description(),
                 todoDTO.status()));
+        history.put(counter++, new Change(ChangeType.POST, newID, todoDTO));
         return repo.findById(newID).orElse(null);
     }
 
@@ -34,12 +45,15 @@ public class TodoService {
 
     public Todo updateById(String id, TodoDTO todoDTO) {
         repo.save(new Todo(id, todoDTO.description(), todoDTO.status()));
+        history.put(counter++, new Change(ChangeType.PUT, id, todoDTO));
         return repo.findById(id).orElse(null);
     }
 
     public Todo deleteById(String id) {
         Todo toBeRemoved = repo.findById(id).orElse(null);
         repo.deleteById(id);
+        history.put(counter++, new Change(ChangeType.DELETE, id,
+                new TodoDTO(toBeRemoved.description(), toBeRemoved.status())));
         return toBeRemoved;
     }
 }
